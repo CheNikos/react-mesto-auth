2986636b80b7ca72c53d58fe9205a134024757a1
CheNikos <chenikos@yandex.ru>
2023-02-06 21:20:44 +0300

    refactor auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,50 +1,38 @@
-export const BASE_URL = 'https://auth.nomoreparties.co/';
+export const BASE_URL = "https://auth.nomoreparties.co/";
 
-function checkResponse(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    Promise.reject(`Ошибка: ${res.status}/${res.statusText}`);
-  };
+const getResponse = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 };
 
-export function register(email, password) {
+export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email, password })
-  })
-    .then(res => checkResponse(res));
+    body: JSON.stringify({ email, password }),
+  }).then(getResponse);
 };
 
-export function authorize(email, password) {
+export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email, password })
-  })
-    .then(res => checkResponse(res))
-    .then((data) => {
-      if (data.token) {
-        const token = data.token;
-        localStorage.setItem('jwt', token);
-
-        return token;
-      };
-    })
+    body: JSON.stringify({ email, password }),
+  }).then(getResponse);
 };
 
-export function getContent(token) {
+export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    }
-  })
-    .then(res => checkResponse(res))
-    .then(data => data)
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(getResponse);
 };
